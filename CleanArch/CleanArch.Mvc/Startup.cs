b7fd73570066b8a728a8916a4f0a9bd2da18d084
luz.galindo <luz.galindo@ipointsystems.com>
2021-08-30 13:46:30 -0500

    Automapper

--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -1,5 +1,6 @@
 using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.IoC;
+using CleanArch.Mvc.Configurations;
 using CleanArch.Mvc.Data;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
@@ -45,6 +46,8 @@ namespace CleanArch.Mvc
             services.AddRazorPages();
 
             services.AddMediatR(typeof(Startup));
+
+            services.RegisterAutomapper();
 
             RegisterServices(services);
         }
